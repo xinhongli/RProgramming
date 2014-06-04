473caedefe81a5177196e1540f5f0643192367b8
pachamaltese <mauriciovargas@ug.uchile.cl>
2014-06-04 14:22:29 -0400

    5 de junio

--- tarea3/desarrollo-tarea3.R
+++ tarea3/desarrollo-tarea3.R
@@ -20,102 +20,105 @@ Init <- function(fileStr, workDirStr = "/Users/pacha/Dropbox/R/RProgramming/tare
 }
 
 best <- function(stateChr, outcomeChr) {
+	#abrir archivo
 	outcomeDfr <- Init("Outcome-of-care-measures.csv")
-	
+	#eliminar warnings y dejar todo en formato numerico
 	suppressWarnings(outcomeDfr[, 11] <- as.numeric(outcomeDfr[, 11]))
 	suppressWarnings(outcomeDfr[, 17] <- as.numeric(outcomeDfr[, 17]))
 	suppressWarnings(outcomeDfr[, 23] <- as.numeric(outcomeDfr[, 23]))
-	
+	#crear tabla de frecuencia por estado y sacar row.names
 	tableDfr <- data.frame(State = names(tapply(outcomeDfr$State, outcomeDfr$State, 
 							length)), Freq = tapply(outcomeDfr$State, outcomeDfr$State, length))
 	rownames(tableDfr) <- NULL
-	
+	#crear tabla de inputs y columnas respectivas
 	inputDfr <- data.frame(Outcome = c("heart attack", "heart failure", "pneumonia"), 
 			Col = c(11, 17, 23))
-	
+	#verificar que los inputs son validos
 	if (nrow(tableDfr[tableDfr$State == stateChr, ]) == 0) 
 		stop("invalid state")
 	if (nrow(inputDfr[inputDfr$Outcome == outcomeChr, ]) == 0) 
 		stop("invalid outcome")
-	
+	#encontrar hospital de menor mortandad ultimo mes en un estado dado
 	stateDfr <- outcomeDfr[outcomeDfr$State == stateChr, ]
 	colNum <- inputDfr[inputDfr$Outcome == outcomeChr, 2]
 	rowNum <- which.min(stateDfr[, colNum])
+	#mostrar mejor hospital de un estado
 	return(stateDfr[rowNum, ]$Hospital.Name)
 }
 
 #PARTE 3
 #3	Ranking hospitals by outcome in a state
 rankhospital <- function(stateChr, outcomeChr, rankObj) {
+	#abrir archivo
 	outcomeDfr <- Init("Outcome-of-care-measures.csv")
-	
+	#eliminar warnings y dejar todo en formato numerico
 	suppressWarnings(outcomeDfr[, 11] <- as.numeric(outcomeDfr[, 11]))
 	suppressWarnings(outcomeDfr[, 17] <- as.numeric(outcomeDfr[, 17]))
 	suppressWarnings(outcomeDfr[, 23] <- as.numeric(outcomeDfr[, 23]))
-	
+	#crear tabla de frecuencia por estado y sacar row.names
 	tableDfr <- data.frame(State = names(tapply(outcomeDfr$State, outcomeDfr$State, 
 							length)), Freq = tapply(outcomeDfr$State, outcomeDfr$State, length))
 	rownames(tableDfr) <- NULL
-	
+	#crear tabla de inputs y columnas respectivas
 	inputDfr <- data.frame(Outcome = c("heart attack", "heart failure", "pneumonia"), 
 			Col = c(11, 17, 23))
-	
+	#verificar que los inputs son validos
 	if (nrow(tableDfr[tableDfr$State == stateChr, ]) == 0) 
 		stop("invalid state")
 	if (nrow(inputDfr[inputDfr$Outcome == outcomeChr, ]) == 0) 
 		stop("invalid outcome")
-	
+	#armar ranking por estado
 	stateDfr <- outcomeDfr[outcomeDfr$State == stateChr, ]
 	colNum <- inputDfr[inputDfr$Outcome == outcomeChr, 2]
 	stateDfr <- stateDfr[complete.cases(stateDfr[, colNum]), ]
 	stateDfr <- stateDfr[order(stateDfr[, colNum], stateDfr$Hospital.Name), 
 	]
-	
+	#convertir ranking a formato numerico y omitir NAs
 	if (rankObj == "best") 
 		rankObj <- 1
 	if (rankObj == "worst") 
 		rankObj <- nrow(stateDfr)
-	
 	suppressWarnings(rankNum <- as.numeric(rankObj))
-	
+	#ranking por estado
 	return(stateDfr[rankNum, ]$Hospital.Name)
 }
 
 #PARTE 4
 #4	Ranking hospitals in all states
 rankall <- function(outcomeChr, rankObj = "best") {
+	#abrir archivo
 	outcomeDfr <- Init("Outcome-of-care-measures.csv")
-	
+	#eliminar warnings y dejar todo en formato numerico
 	suppressWarnings(outcomeDfr[, 11] <- as.numeric(outcomeDfr[, 11]))
 	suppressWarnings(outcomeDfr[, 17] <- as.numeric(outcomeDfr[, 17]))
 	suppressWarnings(outcomeDfr[, 23] <- as.numeric(outcomeDfr[, 23]))
-	
+	#crear tabla de frecuencia por estado y sacar row.names
 	tableDfr <- data.frame(State = names(tapply(outcomeDfr$State, outcomeDfr$State, 
 							length)), Freq = tapply(outcomeDfr$State, outcomeDfr$State, length))
 	rownames(tableDfr) <- NULL
-	
+	#crear tabla de inputs y columnas respectivas
 	inputDfr <- data.frame(Outcome = c("heart attack", "heart failure", "pneumonia"), 
 			Col = c(11, 17, 23))
-	
+	#verificar que los inputs son validos
 	if (nrow(inputDfr[inputDfr$Outcome == outcomeChr, ]) == 0) 
 		stop("invalid outcome")
-	
+	#agregar vector nulo para escribir el ranking sobre las componentes
 	nameChr <- character(0)
-	
+	#construir ranking global
 	for (stateChr in tableDfr$State) {
 		stateDfr <- outcomeDfr[outcomeDfr$State == stateChr, ]
 		colNum <- inputDfr[inputDfr$Outcome == outcomeChr, 2]
 		stateDfr <- stateDfr[complete.cases(stateDfr[, colNum]), ]
 		stateDfr <- stateDfr[order(stateDfr[, colNum], stateDfr$Hospital.Name), 
 		]
-		
+		#convertir ranking a formato numerico y omitir NAs
 		if (rankObj == "best") 
 			rankNum <- 1 else if (rankObj == "worst") 
 			rankNum <- nrow(stateDfr) else suppressWarnings(rankNum <- as.numeric(rankObj))
-		
+		#reemplazar coordenadas del vector nulo
 		nameChr <- c(nameChr, stateDfr[rankNum, ]$Hospital.Name)
 	}
-	
+	#ranking global
 	return(data.frame(hospital = nameChr, state = tableDfr$State))
 }
 
